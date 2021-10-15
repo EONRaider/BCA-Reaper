617cad836fc59aa4b9ae135e3ce26008ed237b03
EONRaider <livewire_voodoo@protonmail.com>
2021-10-15 11:10:55 -0300

    Refactor File class
    Modified the update method signature to
    conform to the new interface of the parent class

--- src/modules/exfiltration/file.py
+++ src/modules/exfiltration/file.py
@@ -11,14 +11,13 @@ from src.modules.exfiltration.base import ExfiltrationModule, ExploitationModule
 class File(ExfiltrationModule):
     def __init__(self, *,
                  module: ExploitationModule,
-                 tag: str = None,
                  file_path: [str, Path]):
-        super().__init__(module, tag)
+        super().__init__(module)
         self.file_path = file_path
 
-    def update(self) -> None:
+    def update(self, message: str) -> None:
         """Write each report on a new line of a text file with the
         specified path."""
         if self.module.has_data is True:
             with open(file=self.file_path, mode="a", encoding="utf_8") as file:
-                file.write(f"{self.report}\n")
+                file.write(f"{message}\n")
