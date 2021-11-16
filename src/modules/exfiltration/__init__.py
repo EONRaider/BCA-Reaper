960f2c2594fff896bb378fb4631cca03be80ae84
EONRaider <livewire_voodoo@protonmail.com>
2021-11-16 12:07:40 -0300

    Modify imports

--- src/modules/exfiltration/__init__.py
+++ src/modules/exfiltration/__init__.py
@@ -1,4 +1,1 @@
 from src.modules.exfiltration.discord_server import Discord
-from src.modules.exfiltration.email import Email
-from src.modules.exfiltration.screen import Screen
-from src.modules.exfiltration.file import File
