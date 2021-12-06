a0446cb6ecf0efd40349c759ed7c7070582753ab
EONRaider <livewire_voodoo@protonmail.com>
2021-12-06 14:04:10 -0300

    Modify build function
    The build mechanism now accepts extended
    arguments for name and placement of generated
    binaries.

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ import PyInstaller.__main__ as pyinstaller
 
 
 def build(args: argparse.Namespace) -> None:
-    """Set-up the arguments required by PyInstaller to build the Reaper
+    """Set up the arguments required by PyInstaller to build the Reaper
     binary."""
 
     config = {
@@ -24,11 +24,16 @@ def build(args: argparse.Namespace) -> None:
             value = value if not isinstance(value, str) else f"'{value}'"
             config_file.write(f"{key} = {value}\n")
 
-    cmd = (
+    name = args.name if args.name is not None else f"{system().lower()}_reaper"
+
+    cmd = [
         "src/reaper.py", "--onefile",
         "--hidden-import", "config",
-        "--name", f"{system().lower()}_reaper"
-    )
+        "--name", name
+    ]
+
+    if args.dest_dir is not None:
+        cmd.append(args.dest_dir)
 
     pyinstaller.run(cmd)
 
@@ -36,4 +41,18 @@ def build(args: argparse.Namespace) -> None:
 if __name__ == "__main__":
     from src.reaper import parse_cli
 
-    build(parse_cli())
+    _args = parse_cli()
+    _args.add_argument(
+        "--name",
+        type=str,
+        help="Name of the generated binary. Defaults to $SYSTEM_reaper, where "
+             "$SYSTEM is the name of the OS where the compilation takes place."
+    )
+    _args.add_argument(
+        "--dest-dir",
+        type=str,
+        help="Absolute path to a directory to write the binary file. Defaults "
+             "to ./dist if unset."
+    )
+
+    build(_args.parse_args())
